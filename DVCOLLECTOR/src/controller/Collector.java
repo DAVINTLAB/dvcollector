f7159eb307e36abd1aa8eeff6ea342320517910d
Max Zorzetti <maxzorzetti@live.com>
2016-12-19 21:44:56 -0200

    Collector now needs to be instantiated.

--- DVCOLLECTOR/src/controller/Collector.java
+++ DVCOLLECTOR/src/controller/Collector.java
@@ -11,45 +11,29 @@ import twitter4j.StatusListener;
 import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import view.StreamController;
 
 public class Collector {
 	
-	public static String filter_args = "hearthstone"; // Separate terms by comma
-	public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-																	//2016-12-07T16:41:26Z
-																	//Wed Aug 27 13:08:45 +0000 2008"
-																	//EEE MMM dd HH:mm:ss Z yyyy
+	private StreamController streamController;
+	private String filter_args = "hearthstone";
 
-	public static void main(String args[]){
+	public Collector(StreamController streamController) {
+		this.streamController = streamController;
+	}
+	
+	/*public static void main(String args[]){
 		try {
 			DatabaseManager.connectToDatabase();
 			startStreamFilter();
 		} catch (TwitterException | IOException e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
-	public static void startStreamFilter() throws TwitterException, IOException{
-	    StatusListener listener = new StatusListener(){
-	        public void onStatus(Status status) {
-	        	long id = status.getId();
-	        	Date created_at = status.getCreatedAt();
-	        	long user_id = status.getUser().getId();
-	        	String user_screenname = status.getUser().getScreenName();
-	        	String text = status.getText();
-	        	
-	        	DatabaseManager.insertStatus(status);
-	        	
-	            //System.out.println(status.getId() + "|" + status.getUser().getName() + " : " + status.getText());
-	        	String tweet = String.format("%s %s %s\n\n", dateFormat.format(created_at), user_screenname, text);
-	        	//System.out.print(tweet);
-	        }
-	        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-	        public void onException(Exception ex) { ex.printStackTrace(); }
-			public void onScrubGeo(long arg0, long arg1) {}
-			public void onStallWarning(StallWarning arg0) {}
-	    };
+	public void startStreamFilter() throws TwitterException, IOException{
+		DatabaseManager.connectToDatabase();
+	    StatusListener listener = new StatusStreamListener(streamController);
 	    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 	    twitterStream.addListener(listener);
 	    twitterStream.filter(filter_args);	    
