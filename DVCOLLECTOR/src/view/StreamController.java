0e37e25c3c9454cb99b3db9e1ed9cf6ff2309db1
Max Zorzetti <maxzorzetti@live.com>
2016-12-19 21:49:14 -0200

    Updated according to the UI changes.

--- DVCOLLECTOR/src/view/StreamController.java
+++ DVCOLLECTOR/src/view/StreamController.java
@@ -1,31 +1,43 @@
 package view;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import twitter4j.Status;
+import twitter4j.TwitterException;
+import util.IsoDateFormatter;
+
+import java.io.IOException;
+
 import controller.*;
 
 public class StreamController {
-
+	
+	@FXML
+	private TextArea tweetTextField;
     @FXML
-    private Label tweetLabel;
+    private TextField userTextField;
     @FXML
-    private Label userLabel;
+    private TextField dateTextField;
     @FXML
     private Button startStreamButton;
     @FXML
     private Button stopStreamButton;
     // Reference to the main application.
     private MainApp mainApp;
+    private Collector collector;
 
     /**
      * The constructor.
      * The constructor is called before the initialize() method.
      */
     public StreamController() {
+    	this.collector = new Collector(this);
     }
 
     /**
@@ -34,6 +46,12 @@ public class StreamController {
      */
     @FXML
     private void initialize() {
+    	try {
+			collector.startStreamFilter();
+		} catch (TwitterException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     /**
@@ -46,8 +64,15 @@ public class StreamController {
     }
     
     public void updateStatus(Status status){
-    	userLabel.setText(status.getUser().getScreenName());
-    	tweetLabel.setText(status.getText());
+    	// Necessary to update the UI without crashing
+    	// TODO Look into this
+    	Platform.runLater(new Runnable(){
+			public void run() { 
+				userTextField.setText(status.getUser().getScreenName());
+				dateTextField.setText(IsoDateFormatter.format(status.getCreatedAt()));
+				tweetTextField.setText(status.getText());
+			}    		   
+    	});
     }
     
 }
