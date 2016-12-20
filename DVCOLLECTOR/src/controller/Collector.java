90b27d42ba7c74303fba35db7ca77c438b02734a
Max Zorzetti <maxzorzetti@live.com>
2016-12-20 02:35:46 -0200

    Added feedback on stream status. Started foundation for custom OAuth keys. Updated UI to reflect changes.

--- DVCOLLECTOR/src/controller/Collector.java
+++ DVCOLLECTOR/src/controller/Collector.java
@@ -4,22 +4,39 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import twitter4j.ConnectionLifeCycleListener;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 import view.StreamController;
 
 public class Collector {
 	
 	private StreamController streamController;
-	private String filter_args = "hearthstone";
+	private String filterArgs = "league of legends, hearthstone";
+	private TwitterStream twitterStream;
 
 	public Collector(StreamController streamController) {
 		this.streamController = streamController;
+	}
+	
+	public Collector(StreamController streamController, String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
+		this.streamController = streamController;
+
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(false);
+		cb.setOAuthConsumerKey(consumerKey);
+		cb.setOAuthConsumerSecret(consumerSecret);
+		cb.setOAuthAccessToken(accessToken);
+		cb.setOAuthAccessTokenSecret(accessTokenSecret);
+		this.twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+		
 	}
 	
 	/*public static void main(String args[]){
@@ -34,8 +51,17 @@ public class Collector {
 	public void startStreamFilter() throws TwitterException, IOException{
 		DatabaseManager.connectToDatabase();
 	    StatusListener listener = new StatusStreamListener(streamController);
-	    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-	    twitterStream.addListener(listener);
-	    twitterStream.filter(filter_args);	    
+	    this.twitterStream = new TwitterStreamFactory().getInstance();
+	    this.twitterStream.addListener(listener);
+	    this.twitterStream.addConnectionLifeCycleListener(new ConnectionLifeCycleListener() {			
+	    	public void onConnect() { streamController.setStreamingStatus(); }
+	    	public void onDisconnect() {}
+			public void onCleanUp() {}
+		});
+	    
+	    this.twitterStream.filter(filterArgs);	    
+	}
+	public void stopStreamFilter(){
+		this.twitterStream.shutdown();
 	}
 }
--- DVCOLLECTOR/src/view/StreamController.java
+++ DVCOLLECTOR/src/view/StreamController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Paint;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import util.IsoDateFormatter;
@@ -19,25 +22,49 @@ import controller.*;
 public class StreamController {
 	
 	@FXML
+	private Button startStreamButton;
+	@FXML
+	private Button stopStreamButton;
+	
+	@FXML
 	private TextArea tweetTextField;
     @FXML
     private TextField userTextField;
     @FXML
     private TextField dateTextField;
+    
     @FXML
-    private Button startStreamButton;
+    private TextField consumerKey;
     @FXML
-    private Button stopStreamButton;
+    private TextField consumerSecret;
+    @FXML
+    private TextField accessToken;
+    @FXML
+    private TextField accessTokenSecret;
+    
+    @FXML
+    private Label status;
+    @FXML
+    private ImageView animation;
+    
     // Reference to the main application.
     private MainApp mainApp;
+    private Image logo;
+    private Image loadAnimation;
+    private Image streamAnimation;
     private Collector collector;
+    private boolean buttonToggle;
 
     /**
      * The constructor.
      * The constructor is called before the initialize() method.
      */
     public StreamController() {
-    	this.collector = new Collector(this);
+    	//this.collector = new Collector(this);
+    	this.logo = new Image("logo.png");
+    	this.loadAnimation = new Image("loading.gif");
+    	this.streamAnimation = new Image("streaming.gif");
+    	this.buttonToggle = true;
     }
 
     /**
@@ -45,13 +72,8 @@ public class StreamController {
      * after the fxml file has been loaded.
      */
     @FXML
-    private void initialize() {
-    	try {
-			collector.startStreamFilter();
-		} catch (TwitterException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    private void initialize() {    	
+    	//setIdleStatus();
     }
 
     /**
@@ -75,4 +97,60 @@ public class StreamController {
     	});
     }
     
+    public void startStream(){
+    	//createCollector();
+    	collector = new Collector(this);
+    	setLoadingStatus();
+    	toggleStreamButtons();
+    	try {
+			collector.startStreamFilter();
+		} catch (TwitterException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private void toggleStreamButtons(){
+    	startStreamButton.setDisable(buttonToggle);
+    	stopStreamButton.setDisable(!buttonToggle);
+    	buttonToggle = !buttonToggle;
+    }
+    
+    private void createCollector() {
+    	String consumerKey = this.consumerKey.getText();
+    	String consumerSecret = this.consumerSecret.getText();
+    	String accessToken = this.accessToken.getText();
+    	String accessTokenSecret = this.accessTokenSecret.getText();
+    	System.out.println(consumerKey);
+    	System.out.println(consumerSecret);
+    	System.out.println(accessToken);
+    	System.out.println(accessTokenSecret);
+		this.collector = new Collector(this, consumerKey, consumerSecret, accessToken, accessTokenSecret);
+	}
+
+	public void stopStream(){
+    	collector.stopStreamFilter();
+    	toggleStreamButtons();
+    	setIdleStatus();
+    }
+    
+    public void setLoadingStatus(){ 
+    	status.setText("Connecting..."); 
+    	status.setTextFill(Paint.valueOf("#b3b300")); // Yellow - has to be a little dark or it gets pretty hard to read
+    	animation.setImage(loadAnimation); 
+    }
+    public void setStreamingStatus(){
+    	Platform.runLater(new Runnable(){
+			public void run() { 
+				status.setText("Receiving stream");
+				status.setTextFill(Paint.valueOf("#006600")); // Green
+				animation.setImage(streamAnimation);
+			}    		   
+    	});
+    }
+    public void setIdleStatus(){
+    	status.setText("Idle");
+    	status.setTextFill(Paint.valueOf("#333333")); // TODO Get the exact default color (this one is pretty close)
+    	animation.setImage(logo);
+    }
 }
